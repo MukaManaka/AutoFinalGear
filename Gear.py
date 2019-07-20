d7520ef07cd4c36c71f93007d3ff47de718db6e8
Rika <569364806@qq.com>
2019-07-20 11:02:15 +0800

    Initial commit

---
+++ Gear.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+class GearCommander(object):
+    """docstring for GearCommander"""
+    def __init__(self):
+        super(GearCommander, self).__init__()
+        pass
+
+
+class Moil(object):
+    """docstring for Moil"""
+    def __init__(self, arg):
+        super(Moil, self).__init__()
+        self.arg = arg
+        
